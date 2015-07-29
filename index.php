45400a06837e224c95071c03c558916700d9c5a1
OlegMix <Olegmixa@mail.ru>
2015-07-29 21:26:25 +0300

    St3

--- index.php
+++ index.php
@@ -3,13 +3,25 @@
 	echo "Его возраст - ".$test."ddd";
 
 ?>
-
+<<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
 	<div class="head">
 			<img src="i.jpg" class="head_img">
-			
 			<div class="head_title">
 				<h1> Название </h1>
 				<input type="text" class="searh" placeholder="поиск"> 
 				<input type="button" class="s_button">
 			</div>
 	</div> 
+	<div class="body">
+	
+	</div>
+</body>
+</html>
+	
+
+
