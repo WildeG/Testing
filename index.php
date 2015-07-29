ed356e5dd67faf8a1c5f4a35e5be86c5aa2d823a
Nicola555 <Nicola555@mail.ru>
2015-07-29 21:27:39 +0300

    st2
    проба2

--- index.php
+++ index.php
@@ -3,3 +3,13 @@
 	echo "Его возраст - ".$test."ddd";
 
 ?>
+
+	<div class="head">
+			<img src="i.jpg" class="head_img">
+			
+			<div class="head_title">
+				<h1> Название </h1>
+				<input type="text" class="searh" placeholder="поиск"> 
+				<input type="button" class="s_button">
+			</div>
+	</div> 
