2ea92781fe03bda852ab019978579219b812730e
Nicola555 <Nicola555@mail.ru>
2015-08-06 18:20:19 +0300

    06/08/2015
    06/08/2015

--- index.php
+++ index.php
@@ -1,24 +1,38 @@
 <?php 
 	$test = 15;
-	echo "Его возраст - ".$test."ddd";
+	/*echo "Его возраст - ".$test. "лет";*/
 
 ?>
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title> пробная работа  </title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
+<body bgcolor="#336699">
+	
+
 	<div class="head">
-			<img src="i.jpg" class="head_img">
+			<h1> <center> <font color="#CC0099" face="Arial"> Из старого – совершенно новое!  </font> </center></h1>
+			<hr width="1200" size="2" ></hr>
+
+			 <p align="center"> <img src="eko-otel.jpg" class="head_img"></p>
 			<div class="head_title">
-				<h1> Название </h1>
-				<input type="text" class="searh" placeholder="поиск"> 
-				<input type="button" class="s_button">
+
+				<p> <font color="#FFCC00" face="Times New" size="5">
+				<i> Costa Verde </i>особенный тем, что размещен в старом самолете Боинг-727. Авиатранспорт был выпушен в 1965 году. 
+				Практически все время своего существования он провел в рейсах, пересекая Атлантику. 
+				Сначала его включили в состав South African Airways. Так продолжалось около 10 лет. 
+				После этого его отправили в Колумбию, в состав Columbia’s Avianca Airline. Но вот срок эксплуатации Боинга подошел к концу. 
+				Одному предпринимателю пришла в голову гениальная идея: выкупить знаменитый самолет, 
+				разобрать его и переправить на побережье Коста-Рики.  </font> </p>
+
+				<!-- <input type="text" class="searh" placeholder="поиск"> -->
+				<!--<input type="button" class="s_button"> -->
 			</div>
 	</div> 
-	<div class="body">
 	
+	<div class="body">
 	</div>
 </body>
 </html>
